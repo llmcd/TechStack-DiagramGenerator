9466787037bf009b1a4e30cdfba1cee7f477ffa8
llmcd <7576641+llmcd@users.noreply.github.com>
2024-12-21 15:19:17 -0500

    Delete src/components directory

--- src/components/support/BuyMeCoffeeTracker.ts
+++
@@ -1,9 +0,0 @@
-export const trackBuyMeCoffeeClick = () => {
-  // If you have analytics set up, track the event
-  if (window.gtag) {
-    window.gtag('event', 'click', {
-      event_category: 'Support',
-      event_label: 'Buy Me a Coffee'
-    });
-  }
-};
